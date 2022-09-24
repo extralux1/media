e3074901528cd00eca3abe4349d6c5e3d7a0c0fb
extralux1 <112030751+extralux1@users.noreply.github.com>
2022-09-24 16:42:49 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+<head><meta charset="us-ascii">
+	<title>Extra Lux</title>
+</head>
+<body>
+<p style="text-align: center;"><img alt="" src="/11_flash.png.gif" style="width: 60px; height: 60px;" /></p>
+
+<p style="text-align: center;">RADIO LUX STRANICA PLAYERA</p>
+
+<p style="text-align: center;"><a href="https://serverelvir.com" target="_blank"><img alt="EBM SERVER" src="/EBM-SLIKA.jpg" style="width: 300px; height: 300px;" /></a></p>
+
+<p><iframe height="110" scrolling="no" src="https://str.bem-streaming.tk/cp/widgets/player/single/?p=8010" style="border:none;" width="100%"></iframe></p>
+</div>
+<div style="position: absolute;  
+vertical-align: top;  
+text-align: left;  
+top: 586px;  
+left: 0px;">
+<img src="/playery.png" width="1245" height="645" title="Lux" /></a>
+
+<p style="text-align: center;"></p>
+</body>
+</html>
